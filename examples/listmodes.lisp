dc9017b60bec384e14382541650fb7d1e752f80f
James Ashley <james@teamdaves.us>
2013-04-27 02:18:43 -0500

    Note re: mode listing failure

--- examples/listmodes.lisp
+++ examples/listmodes.lisp
@@ -1,7 +1,14 @@
 (require '#:asdf)
 (asdf:oos 'asdf:load-op '#:cl-glfw)
 
-(glfw:with-init 
+(glfw:with-init
+  ;; The next line fails. 
+  ;; Q: Because we haven't created a GL window yet?
+  ;(format t "OpenGL version: ~A~%" (glfw:get-gl-version))
+  ;; A: Yes:
+  (glfw:with-open-window ()
+      (format t "OpenGL version: ~A~%" (glfw:get-gl-version)))
+
   (destructuring-bind (width height redbits greenbits bluebits) (glfw:get-desktop-mode) 
     (format t "Desktop mode: ~d x ~d x ~d~%" width height (+ redbits greenbits bluebits)))
 
