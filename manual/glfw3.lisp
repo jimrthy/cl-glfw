6692efcb51f35a2de65ae8d67e81ba6eba6310b6
James Ashley <james@teamdaves.us>
2013-05-02 21:37:15 -0500

    Have the basic monitor-querying functions working. Totally ignoring the rest of that part of the interface

--- manual/glfw3.lisp
+++ manual/glfw3.lisp
@@ -17,6 +17,7 @@
 ;; N.B.:
 ;; Do not forget to (close-foreign-library libglfw)!!
 
+;; It very well might be using something like grovel to pick up more details.
 (defctype glfw-window :pointer)
 
 ;;; Booleans...this is more than a little lame
@@ -72,16 +73,8 @@
   (minor :pointer :int)
   (rev   :pointer :int))
 
-#+null(defun glfw-get-version ()
-  "Return a list of major-minor-revision"
-  (let (parameters)
-    (unwind-protect 
-	 (let ((parameters (foreign-alloc :int :count 3)))
-	   (glfw-get-version-official parameters (inc-pointer parameters 1) (inc-pointer parameters 2))
-	   (list (mem-aptr parameters :int 0) (mem-aptr parameters :int 1) (mem-aptr parameters :int 2)))
-      (when parameters
-	(foreign-free parameters)))))
 (defun glfw-get-version ()
+  ""
   (with-foreign-object (array :int 3)
     (glfw-get-version-official array (inc-pointer array 1) (inc-pointer array 2))
     (list (mem-aref array :int) (mem-aref array :int 1) (mem-aref array :int 2))))
@@ -92,10 +85,62 @@
 (defcfun ("glfwGetVersionString" glfw-get-version-string)
     :string)
 
+;;; Here's where things start getting interesting.
 ;;; Following along with the header, we get into things like error callbacks
 ;;; and the "main monitor."
-(error "Here's where things start getting interesting.")
 
+;; This looks as though it works. It hasn't actually been tested by causing
+;; an error yet.
+;; FIXME: How do I define a C function pointer type?
+;(defctype glfw-error-fun :void (error-code :int) (description :string))
+    "new-callback is a function that accepts an int error-code and const char* description.
+The description string is only valid within the scope of the callback.
+Returns the previous callback on success, NULL on failure.
+Runs in the context of whichever thread caused the error."
+(defcfun ("glfwSetErrorCallback" glfw-set-error-callback)
+    :pointer
+  (new-callback :pointer))
+(defcallback default-error-handler :void ((error-code :int) (description :pointer :char))
+  (format nil "Error Code ~A: ~A~%" error-code description))
+(defparameter *original-error-handler* (glfw-set-error-callback (callback default-error-handler)))
+
+;;; FIXME: That really feels like it's just begging for a multi-method.
+;;; It obviously doesn't, but something feels off.
+;;; At the very least, it should be dead simple to swap in more meaningful handlers.
+;;; It's something at least vaguely interesting to consider.
+
+;;; Monitor information
+
+;;; FIXME: Have to define the GLFWmonitor struct
+
+;; Returns an array of handles for all currently connected monitors.
+;; Only valid until the monitor configuration changes
+;; Returns NULL on error
+(defcfun ("glfwGetMonitors" glfw-get-monitors-native)
+    :pointer
+    (count :pointer :int))
+
+#+null(defcstruct glfw-monitor 
+  "Describe one of the monitors attached to the system.
+FIXME: Really should use groveller instead, if I understand this at all.
+Especially since it's intentionally opaque in the header.
+For now, I really don't need anything except the size."
+)
+;; This returns a pair of monitor* on my system, which seems totally correct.
+;; No idea whether they're valid or meaningful.
+(defun glfw-get-monitors ()
+  (let ((count (foreign-alloc :int)))
+    (unwind-protect 
+	 (let ((monitors (glfw-get-monitors-native count))
+	       (result ()))
+	   (dotimes (i (mem-ref count :int))
+	     (push (mem-aref monitors #|glfw-monitor|# :pointer i) result))
+	   result)
+      (foreign-free count))))
+
+(defcfun ("glfwGetPrimaryMonitor" glfw-get-primary-monitor)
+    :pointer)
+			 
 ;;; Termination
 
 ;; Closes all threads.
