8c12836b74fbbd5a69dc9cdf2b750d5a543e05e1
James Ashley <james@teamdaves.us>
2013-05-05 17:26:19 -0500

    Further debugging, trying to figure out why my simple example just is not working.

--- examples/3/simple.lisp
+++ examples/3/simple.lisp
@@ -9,7 +9,7 @@
 (asdf:oos 'asdf:load-op '#:cl-opengl)
 
 (defpackage #:glfw3-example
-  (:use #:cl #:cl-glfw3))
+  (:use #:cl #:cl-glfw3 #:cl-opengl))
 (in-package #:glfw3-example)
 
 ;;; These next really should be wrapped inside unwind-protect.
@@ -20,10 +20,23 @@
 (glfw3::make-context-current *simple-window*)
 
 (defun setup-view ()
+  ;;; I'm getting hung up here apparently. WTF?
   (gl:matrix-mode :projection)
   (gl:load-identity)
   (gl:ortho 0 1 0 1 -1 1))
+;;; Defining that previous function, followed by calling it from the REPL, causes
+;;; an error about passing an invalid float value to something.
+;;; Just eval-ing the next line from emacs seems to hang me up in an infinite loop.
 (setup-view)
+;;; Running each of the commands within setup-view individually, from the REPL,
+;;; seems to run absolutely fine.
+
+(defun key-callback (window key-code action)
+  (when (and (eql action glfw3::*glfw-press*)
+	     (eql key-code 256))
+    (glfw3::set-window-should-close window t)))
+;;; This next line definitely fails. The callback value isn't a MACPTR.
+(glfw3::set-key-callback *simple-window* #'key-callback)
 
 (defun draw ()
   (gl:clear :color-buffer)
@@ -35,12 +48,18 @@
     (gl:color 1 0 0) (gl:vertex  1  0 0)
     (gl:color 0 1 0) (gl:vertex -1  1 0)
     (gl:color 0 0 1) (gl:vertex -1 -1 0))
+  ;; FIXME: Debug only.
+  ;; Just so I have some evidence that something's happening.
+  (format t "Frame")
   (gl:flush))
 
 ;;; The problem's pretty obviously here:
 ;;; There's no exit condition.
 (defun run ()
   (loop named do-open-window do
+       ;; Another problem with this current implementation:
+       ;; Nothing's actually getting drawn.
+       ;; At least, nothing visible.
        (draw)
        (glfw3:swap-buffers *simple-window*)
        (glfw3:poll-events)
@@ -50,5 +69,5 @@
 (run)
 
 (setf *simple-window* nil)
-(glfw3:terminate)
-(glfw3:clean-up)
+(glfw3::terminate)
+(glfw3::clean-up)
