80dd6b60176fca582f7ebf53a2380f47f9375ed8
James Ashley <james@teamdaves.us>
2013-05-15 19:06:28 -0500

    Breaking into smaller pieces so I can try to get a glimpse of what's going on

--- examples/3/barest.lisp
+++ examples/3/barest.lisp
@@ -14,45 +14,49 @@
   (:use #:cl #:cl-glfw3 #:cl-opengl))
 (in-package #:glfw3-example)
 
-;; FAIL: Can't translate the lambda() to a MACPTR
 (glfw3:set-error-callback (lambda (error-code description)
 			    (format t "Error ~A: '~A'~%" error-code description)))
 
+(defun event-loop (window)
+  (do ()
+      ((glfw3:window-should-close-p window) t)
+    (progn
+      ;; The event loop
+      (let ((time (glfw3:get-time)))
+	(gl:clear :color-buffer)
+	(gl:matrix-mode :modelview)
+	(gl:rotate (* time 50) 0 0 1)
+		   
+	;; Honestly, this seems to belong in a with-macro as well.
+	(gl:begin :triangles)
+	(gl:color 1 0 0)
+	(gl:vertex -0.6 -0.4 0)
+	(gl:color 0 1 0)
+	(gl:vertex 0.6 -0.4 0)
+	(gl:color 0 0 1)
+	(gl:vertex 0 0.6 0)
+	(gl:end)
+
+	(glfw3:poll-events)
+	(glfw3:swap-buffers window)))))
+
+(defun run (window)
+  (progn 
+    (glfw3:make-context-current window)
+    
+    ;; Have to have the OpenGL context before we can do this
+    (glfw3:set-window-size-callback window (lambda (window width height)
+					     (gl:viewport 0 0 width height)
+					     (gl:matrix-mode :projection)
+					     (gl:load-identity)
+					     (let ((ratio (/ width height)))
+					       (gl:ortho (- ratio) ratio -1 1 1 -1))))
+    (event-loop window)))
 
 (if (glfw3:glfw-init)
     (let ((window (glfw3:create-window 640 480 "My Title" (glfw3:*null*) (glfw3:*null*))))
       (unwind-protect ; pretty obviously, protect using a with- macro instead.
-	   (progn 
-	     (glfw3:make-context-current window)
-
-	     ;; Have to have the OpenGL context before we can do this
-	     (glfw3:set-window-size-callback window (lambda (window width height)
-						      (gl:viewport 0 0 width height)
-						      (gl:matrix-mode :projection)
-						      (gl:load-identity)
-						      (let ((ratio (/ width height)))
-							(gl:ortho (- ratio) ratio -1 1 1 -1))))
-	     (do ()
-		 ((glfw3:window-should-close-p window) t)
-	       (progn
-		 ;; The event loop
-		 (let ((time (glfw3:get-time)))
-		   (gl:clear :color-buffer)
-		   (gl:matrix-mode :modelview)
-		   (gl:rotate (* time 50) 0 0 1)
-		   
-		   ;; Honestly, this seems to belong in a with-macro as well.
-		   (gl:begin :triangles)
-		   (gl:color 1 0 0)
-		   (gl:vertex -0.6 -0.4 0)
-		   (gl:color 0 1 0)
-		   (gl:vertex 0.6 -0.4 0)
-		   (gl:color 0 0 1)
-		   (gl:vertex 0 0.6 0)
-		   (gl:end)
-
-		   (glfw3:poll-events)
-		   (glfw3:swap-buffers window)))))
+	   (run)
 	(glfw3:destroy-window window)))
     (format t "Failed to initialize"))
 
